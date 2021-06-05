2dffdd07a50663e9dca5e999417aa6444fa2c679
RedGrapefruit <karpovanton729@gmail.com>
2021-06-05 10:44:38 +0300

    Add a serializable JSON module config

--- src/main/kotlin/com/redgrapefruit/utopia/Utopia.kt
+++ src/main/kotlin/com/redgrapefruit/utopia/Utopia.kt
@@ -1,7 +1,21 @@
 package com.redgrapefruit.utopia
 
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.redgrapefruit.utopia.module.ModuleConfig
 import net.fabricmc.api.ModInitializer
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
 
 class Utopia : ModInitializer {
-    override fun onInitialize() {}
+    override fun onInitialize() {
+        // First, initialize the ModuleConfig
+        ModuleConfig.init()
+    }
+
+    companion object {
+        val LOG: Logger = LogManager.getLogger()
+
+        val GSON: Gson = GsonBuilder().setPrettyPrinting().create()
+    }
 }
--- src/main/kotlin/com/redgrapefruit/utopia/module/ModuleConfig.kt
+++ src/main/kotlin/com/redgrapefruit/utopia/module/ModuleConfig.kt
@@ -1,5 +1,10 @@
 package com.redgrapefruit.utopia.module
 
+import com.google.gson.JsonObject
+import com.redgrapefruit.utopia.Utopia
+import net.fabricmc.loader.FabricLoader
+import java.io.*
+
 /**
  * A (de)serialized configuration file with all enabled [Module]s.
  *
@@ -11,5 +16,87 @@ object ModuleConfig {
      */
     internal val enabledModules: MutableList<Module> = mutableListOf()
 
+    /**
+     * Initializes the configuration file
+     */
+    fun init() {
+        // Get the Minecraft config directory (on Windows 10 C:/Users/$USERNAME$/AppData/Local/.minecraft/config)
+        val configDir: File = FabricLoader.INSTANCE.configDir.toFile()
+        // If it doesn't exist, create it
+        if (!configDir.exists()) configDir.mkdirs()
 
+        // Create the module settings JSON
+        val optionsFile: File = File(configDir, "utopia.modules.json")
+        // If the JSON doesn't exist, create it and fill it up with default values
+        if (!optionsFile.exists()) {
+            try {
+                val writer = FileWriter(optionsFile)
+                setToDefault(writer)
+                return
+            } catch (exception: Exception) {
+                // Error out if couldn't create the FileWriter
+                Utopia.LOG.error("Couldn't create FileWriter for the options file with the following stack trace:")
+                exception.printStackTrace()
+                return
+            }
+        }
+
+        // If we are here, we assume that the file already contained something, so we read its contents
+        // Create a FileReader
+        try {
+            val reader = FileReader(optionsFile)
+            setToExisting(reader)
+        } catch (exception: Exception) {
+            // Error out if couldn't create the FileReader
+            Utopia.LOG.error("Couldn't create FileReader for the options file with the following stack trace:")
+            exception.printStackTrace()
+        }
+    }
+
+    /**
+     * Sets the values to already existing in the configuration file
+     * @param reader [Reader] for this file
+     */
+    private fun setToExisting(reader: Reader) {
+        val jsonObject = Utopia.GSON.fromJson(reader, JsonObject::class.java)
+
+        // Initialize each module with the value from the JsonObject
+        set("realism", Module.REALISM, jsonObject)
+        set("furniture", Module.FURNITURE, jsonObject)
+        set("exploration", Module.EXPLORATION, jsonObject)
+        set("building", Module.BUILDING, jsonObject)
+        set("cooking", Module.COOKING, jsonObject)
+    }
+
+    /**
+     * Sets the values in the configuration file to defaults (all true)
+     * @param writer [Writer] for this file
+     */
+    private fun setToDefault(writer: Writer) {
+        val jsonObject = JsonObject()
+
+        // Initialize each module with true
+        enable("realism", Module.REALISM, jsonObject)
+        enable("furniture", Module.FURNITURE, jsonObject)
+        enable("exploration", Module.EXPLORATION, jsonObject)
+        enable("building", Module.BUILDING, jsonObject)
+        enable("cooking", Module.COOKING, jsonObject)
+
+        // Write to file
+        Utopia.GSON.toJson(jsonObject, writer)
+
+        writer.close()
+    }
+
+    private fun enable(name: String, module: Module, jsonObject: JsonObject) {
+        jsonObject.addProperty(name, true)
+
+        enabledModules.add(module)
+    }
+
+    private fun set(name: String, module: Module, jsonObject: JsonObject) {
+        val enabled = jsonObject.get(name).asBoolean
+
+        if (enabled) enabledModules.add(module)
+    }
 }
