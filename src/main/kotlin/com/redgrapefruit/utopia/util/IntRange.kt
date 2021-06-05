9157caf1770949b2dca885e0d861c52229b2fef9
RedGrapefruit <karpovanton729@gmail.com>
2021-06-05 14:11:10 +0300

    Add @SetModule definition to IntRange

--- src/main/kotlin/com/redgrapefruit/utopia/util/IntRange.kt
+++ src/main/kotlin/com/redgrapefruit/utopia/util/IntRange.kt
@@ -1,10 +1,13 @@
 package com.redgrapefruit.utopia.util
 
 import com.redgrapefruit.utopia.Utopia
+import com.redgrapefruit.utopia.module.Module
+import com.redgrapefruit.utopia.module.SetModule
 
 /**
  * A range of integers to randomly pick from
  */
+@SetModule(Module.REALISM)
 class IntRange(
     /**
      * Minimum value of the resulting int
