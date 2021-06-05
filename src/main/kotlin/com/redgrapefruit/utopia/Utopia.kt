d43451c334ff94eccf880cf41f9d1dc83adb2bca
RedGrapefruit <karpovanton729@gmail.com>
2021-06-05 09:06:51 +0300

    Convert to Kotlin

---
+++ src/main/kotlin/com/redgrapefruit/utopia/Utopia.kt
@@ -0,0 +1,7 @@
+package com.redgrapefruit.utopia
+
+import net.fabricmc.api.ModInitializer
+
+class Utopia : ModInitializer {
+    override fun onInitialize() {}
+}
---
+++ src/main/kotlin/com/redgrapefruit/utopia/client/UtopiaClient.kt
@@ -0,0 +1,10 @@
+package com.redgrapefruit.utopia.client
+
+import net.fabricmc.api.ClientModInitializer
+import net.fabricmc.api.EnvType
+import net.fabricmc.api.Environment
+
+@Environment(EnvType.CLIENT)
+class UtopiaClient : ClientModInitializer {
+    override fun onInitializeClient() {}
+}
