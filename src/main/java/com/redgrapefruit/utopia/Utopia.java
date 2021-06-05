8b1f29807f4a75c291e1ceceeef4fd8642ea45ed
RedGrapefruit <karpovanton729@gmail.com>
2021-06-05 08:30:51 +0300

    Initial commit

---
+++ src/main/java/com/redgrapefruit/utopia/Utopia.java
@@ -0,0 +1,10 @@
+package com.redgrapefruit.utopia;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Utopia implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/com/redgrapefruit/utopia/client/UtopiaClient.java
@@ -0,0 +1,13 @@
+package com.redgrapefruit.utopia.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class UtopiaClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
