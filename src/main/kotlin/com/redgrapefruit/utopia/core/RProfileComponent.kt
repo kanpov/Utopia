f7cfe96f7f08024e5d271073f1b0a50af0101712
RedGrapefruit <karpovanton729@gmail.com>
2021-06-06 16:15:13 +0300

    Remove CCA; we're doing this the mixin way

--- src/main/kotlin/com/redgrapefruit/utopia/core/RProfileComponent.kt
+++ src/main/kotlin/com/redgrapefruit/utopia/core/RProfileComponent.kt
@@ -13,26 +13,10 @@ const val MIN_TICK_LOSS: Int = 20
 /**
  * Contains changed-at-runtime values of a food type and serializes them (CCA).
  */
-class RProfileComponent(
+data class RProfileComponent(
     var rotProgress: Int = 0,
     var overdueProgress: Int = 0,
     var previousTick: Long = 0L,
     var isInitialized: Boolean = false,
     var fridgeState: RFridgeState = RFridgeState.NOT_IN_FRIDGE
-) : Component {
-    override fun readFromNbt(tag: CompoundTag) {
-        rotProgress = tag.getInt("Rot Progress")
-        overdueProgress = tag.getInt("Overdue Progress")
-        previousTick = tag.getLong("Previous Tick")
-        isInitialized = tag.getBoolean("Is Initialized")
-        fridgeState = RFridgeState.fromTag("Fridge State", tag)
-    }
-
-    override fun writeToNbt(tag: CompoundTag) {
-        tag.putInt("Rot Progress", rotProgress)
-        tag.putInt("Overdue Progress", overdueProgress)
-        tag.putLong("Previous Tick", previousTick)
-        tag.putBoolean("Is Initialized", isInitialized)
-        RFridgeState.toTag("Fridge State", fridgeState, tag)
-    }
-}
+)
