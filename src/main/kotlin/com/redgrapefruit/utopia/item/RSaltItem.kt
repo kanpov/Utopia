4dcb480263a296b3115fa528819027f300d6934c
RedGrapefruit <karpovanton729@gmail.com>
2021-06-07 07:50:48 +0300

    Add salt item

---
+++ src/main/kotlin/com/redgrapefruit/utopia/item/RSaltItem.kt
@@ -0,0 +1,9 @@
+package com.redgrapefruit.utopia.item
+
+import com.redgrapefruit.utopia.GROUP
+import net.minecraft.item.Item
+
+/**
+ * Salt used for crafting [RSaltedFoodItem]s
+ */
+class RSaltItem : Item(Settings().group(GROUP))
