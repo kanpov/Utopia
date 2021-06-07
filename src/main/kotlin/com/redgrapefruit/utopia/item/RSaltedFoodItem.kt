3bd82accb6e5fb7939bdd239ead4a2f12b3340d7
RedGrapefruit <karpovanton729@gmail.com>
2021-06-07 07:46:00 +0300

    Add salted variants

---
+++ src/main/kotlin/com/redgrapefruit/utopia/item/RSaltedFoodItem.kt
@@ -0,0 +1,26 @@
+package com.redgrapefruit.utopia.item
+
+import com.redgrapefruit.utopia.GROUP
+import com.redgrapefruit.utopia.core.RFoodConfig
+import com.redgrapefruit.utopia.core.RFoodState
+import com.redgrapefruit.utopia.util.overrideComponentValues
+import net.minecraft.item.FoodComponent
+
+/**
+ * A salted variant of a [RFoodItem]
+ */
+class RSaltedFoodItem : RFoodItem {
+    constructor(config: RFoodConfig) : super(config) {
+        state = RFoodState.SALTED
+    }
+
+    constructor(config: RFoodConfig, component: FoodComponent) : super(config, GROUP, {
+        val builder = FoodComponent.Builder()
+
+        overrideComponentValues(component, builder, 0, 0f)
+
+        builder.build()
+    }) {
+        state = RFoodState.SALTED
+    }
+}
