bf88a1471531a0df943327220b81f5eed42947bc
RedGrapefruit <karpovanton729@gmail.com>
2021-06-08 17:52:41 +0300

    Fix unregistered fridge screen

--- src/main/kotlin/com/redgrapefruit/utopia/client/registry/FScreenRegistry.kt
+++ src/main/kotlin/com/redgrapefruit/utopia/client/registry/FScreenRegistry.kt
@@ -1,13 +1,16 @@
 package com.redgrapefruit.utopia.client.registry
 
+import com.redgrapefruit.utopia.client.screen.FFridgeScreen
 import com.redgrapefruit.utopia.common.module.Module
 import com.redgrapefruit.utopia.common.module.ModuleFeature
+import com.redgrapefruit.utopia.common.registry.FScreenHandlerRegistry
+import net.fabricmc.fabric.api.client.screenhandler.v1.ScreenRegistry
 
 object FScreenRegistry : ModuleFeature {
     override val module: Module = Module.FURNITURE
     override val isClientSide: Boolean = true
 
     override fun run() {
-
+        ScreenRegistry.register(FScreenHandlerRegistry.FRIDGE_SCREEN_HANDLER, ::FFridgeScreen)
     }
 }
