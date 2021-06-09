e3bb5720703f2045be6876f41b9f9f245756dac7
RedGrapefruit <karpovanton729@gmail.com>
2021-06-09 09:57:24 +0300

    Finish almond coverage

--- src/main/kotlin/com/redgrapefruit/utopia/common/registry/RConfigRegistry.kt
+++ src/main/kotlin/com/redgrapefruit/utopia/common/registry/RConfigRegistry.kt
@@ -2,6 +2,9 @@ package com.redgrapefruit.utopia.common.registry
 
 import com.redgrapefruit.utopia.common.core.data.RFoodCategory
 import com.redgrapefruit.utopia.common.core.data.RFoodConfig
+import com.redgrapefruit.utopia.common.core.data.RFoodEffectConfig
+import com.redgrapefruit.utopia.common.util.RIntRange
+import net.minecraft.entity.effect.StatusEffects
 
 /**
  * Contains all the food configs
@@ -11,6 +14,13 @@ object RConfigRegistry {
         overdueSpeed = 7,
         overdueState = 100000,
         category = RFoodCategory.NUT,
-        effects = emptySet()
+        effects = mutableSetOf(
+            RFoodEffectConfig(
+                statusEffect = StatusEffects.SPEED,
+                durationRange = RIntRange(100, 200),
+                amplifier = 0,
+                chance = 0.6f
+            )
+        )
     )
 }
