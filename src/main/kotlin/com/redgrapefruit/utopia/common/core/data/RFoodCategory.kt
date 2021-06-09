5053b4d76d741e1b4fd3c90ae9b3424bb1a0eac7
RedGrapefruit <karpovanton729@gmail.com>
2021-06-09 18:15:49 +0300

    Add a plant category

--- src/main/kotlin/com/redgrapefruit/utopia/common/core/data/RFoodCategory.kt
+++ src/main/kotlin/com/redgrapefruit/utopia/common/core/data/RFoodCategory.kt
@@ -39,6 +39,7 @@ enum class RFoodCategory(
     BERRIES(1, 0.6f, "Berries", false, true, false, "Small and tasty berries."),
     FISH(3, 1.25f, "Fish", false, true, true, "Nice and yummy fish."),
     NUT(2, 1.1f, "Nut", false, true, false, "Crunchy nuts."),
+    PLANT(1, 0.8f, "Plant", false, true, false, "An edible plant."),
     PRODUCT(3, 1f, "Product", false, true, true, "Cooked product - bakery, soup, stew etc."),
     MISCELLANEOUS(2, 0.75f, "Miscellaneous", false, true, false, "Food that doesn't fit in regular categories")
 }
