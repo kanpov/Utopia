08fd8dd96e3a22ccfe2e42e65d0a5f4acad93620
RedGrapefruit <karpovanton729@gmail.com>
2021-06-09 11:38:04 +0300

    Add recipes for salted variants

--- src/main/kotlin/com/redgrapefruit/utopia/common/item/RSaltItem.kt
+++
@@ -1,9 +0,0 @@
-package com.redgrapefruit.utopia.common.item
-
-import com.redgrapefruit.utopia.common.GROUP
-import net.minecraft.item.Item
-
-/**
- * Salt used for crafting [RSaltedFoodItem]s
- */
-class RSaltItem : Item(Settings().group(GROUP))
