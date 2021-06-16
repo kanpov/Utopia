482dd148c464d525a8afbff88b0fd538643352d4
RedGrapefruit <karpovanton729@gmail.com>
2021-06-16 09:28:42 +0300

    Create a very simple DSL for FoodConfigs

--- src/main/kotlin/com/redgrapefruit/utopia/common/core/data/FoodConfig.kt
+++ src/main/kotlin/com/redgrapefruit/utopia/common/core/data/FoodConfig.kt
@@ -10,37 +10,45 @@ data class FoodConfig(
     /**
      * The hunger of this food type. Only used in new food
      */
-    val hunger: Int = UNUSED_PROPERTY,
+    var hunger: Int = UNUSED_PROPERTY,
     /**
      * The saturation modifier of this food type. Only used in new food
      */
-    val saturationModifier: Float = UNUSED_PROPERTY_FLOAT,
+    var saturationModifier: Float = UNUSED_PROPERTY_FLOAT,
     /**
      * The state of the rot property in which the food is considered rotten
      */
-    val rotState: Int = UNUSED_PROPERTY,
+    var rotState: Int = UNUSED_PROPERTY,
     /**
      * The amount of rot property that is incremented every tick of the game
      */
-    val rotSpeed: Int = UNUSED_PROPERTY,
+    var rotSpeed: Int = UNUSED_PROPERTY,
     /**
      * The state of the overdue property in which the food is considered overdue
      */
-    val overdueState: Int = UNUSED_PROPERTY,
+    var overdueState: Int = UNUSED_PROPERTY,
     /**
      * The amount of overdue property that is incremented every tick of the game
      */
-    val overdueSpeed: Int = UNUSED_PROPERTY,
+    var overdueSpeed: Int = UNUSED_PROPERTY,
     /**
      * The efficiency of using salt on this food type
      */
-    val saltEfficiency: Int = UNUSED_PROPERTY,
+    var saltEfficiency: Int = UNUSED_PROPERTY,
     /**
      * The efficiency of putting this food type inside of a fridge
      */
-    val fridgeEfficiency: Int = UNUSED_PROPERTY,
+    var fridgeEfficiency: Int = UNUSED_PROPERTY,
     /**
      * The [FoodCategory] that this food type belongs to
      */
-    val category: FoodCategory
+    var category: FoodCategory = FoodCategory.MISCELLANEOUS
 )
+
+// DSL
+
+fun config(init: FoodConfig.() -> Unit): FoodConfig {
+    val instance = FoodConfig()
+    instance.init()
+    return instance
+}
