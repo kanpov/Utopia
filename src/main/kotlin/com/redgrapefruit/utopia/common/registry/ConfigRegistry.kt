b433b10c31f19a4da74ed912071cd7962d8303c1
RedGrapefruit <karpovanton729@gmail.com>
2021-06-16 09:46:36 +0300

    Port ConfigRegistry to the new DSL

--- src/main/kotlin/com/redgrapefruit/utopia/common/registry/ConfigRegistry.kt
+++ src/main/kotlin/com/redgrapefruit/utopia/common/registry/ConfigRegistry.kt
@@ -1,240 +1,240 @@
 package com.redgrapefruit.utopia.common.registry
 
 import com.redgrapefruit.utopia.common.core.FoodCategory
-import com.redgrapefruit.utopia.common.core.FoodConfig
+import com.redgrapefruit.utopia.common.core.config
 
 /**
  * Contains all the food configs
  */
 object ConfigRegistry {
-    val ALMOND = FoodConfig(
-        overdueSpeed = 7,
-        overdueState = 100000,
-        saltEfficiency = 4,
+    val ALMOND = config {
+        overdueSpeed = 7
+        overdueState = 100000
+        saltEfficiency = 4
         category = FoodCategory.NUT
-    )
+    }
 
-    val ALMOND_BRITTLE = FoodConfig(
-        overdueSpeed = 8,
-        overdueState = 120000,
-        fridgeEfficiency = 5,
-        saltEfficiency = 2,
+    val ALMOND_BRITTLE = config {
+        overdueSpeed = 8
+        overdueState = 120000
+        fridgeEfficiency = 5
+        saltEfficiency = 2
         category = FoodCategory.PRODUCT
-    )
+    }
 
-    val APPLE_JUICE = FoodConfig(
-        overdueSpeed = 11,
-        overdueState = 100000,
-        fridgeEfficiency = 6,
+    val APPLE_JUICE = config {
+        overdueSpeed = 11
+        overdueState = 100000
+        fridgeEfficiency = 6
         category = FoodCategory.PRODUCT
-    )
+    }
 
-    val APPLE_PIE = FoodConfig(
-        overdueSpeed = 5,
-        overdueState = 70000,
-        fridgeEfficiency = 2,
+    val APPLE_PIE = config {
+        overdueSpeed = 5
+        overdueState = 70000
+        fridgeEfficiency = 2
         category = FoodCategory.PRODUCT
-    )
+    }
 
-    val APRICOT = FoodConfig(
-        overdueSpeed = 9,
-        overdueState = 135000,
-        fridgeEfficiency = 5,
-        saltEfficiency = 4,
+    val APRICOT = config {
+        overdueSpeed = 9
+        overdueState = 135000
+        fridgeEfficiency = 5
+        saltEfficiency = 4
         category = FoodCategory.FRUIT
-    )
+    }
 
-    val APRICOT_JAM = FoodConfig(
-        overdueSpeed = 6,
-        overdueState = 85000,
-        fridgeEfficiency = 2,
+    val APRICOT_JAM = config {
+        overdueSpeed = 6
+        overdueState = 85000
+        fridgeEfficiency = 2
         category = FoodCategory.PRODUCT
-    )
+    }
 
-    val ARTICHOKE = FoodConfig(
-        overdueSpeed = 4,
-        overdueState = 60000,
-        fridgeEfficiency = 1,
-        saltEfficiency = 2,
+    val ARTICHOKE = config {
+        overdueSpeed = 4
+        overdueState = 60000
+        fridgeEfficiency = 1
+        saltEfficiency = 2
         category = FoodCategory.PLANT
-    )
+    }
 
-    val ARTICHOKE_DIP = FoodConfig(
-        overdueSpeed = 5,
-        overdueState = 80000,
-        fridgeEfficiency = 2,
-        saltEfficiency = 3,
+    val ARTICHOKE_DIP = config {
+        overdueSpeed = 5
+        overdueState = 80000
+        fridgeEfficiency = 2
+        saltEfficiency = 3
         category = FoodCategory.PRODUCT
-    )
+    }
 
-    val ASPARAGUS = FoodConfig(
-        overdueSpeed = 9,
-        overdueState = 135000,
-        fridgeEfficiency = 5,
-        saltEfficiency = 4,
+    val ASPARAGUS = config {
+        overdueSpeed = 9
+        overdueState = 135000
+        fridgeEfficiency = 5
+        saltEfficiency = 4
         category = FoodCategory.PLANT
-    )
+    }
 
-    val AVOCADO = FoodConfig(
-        overdueSpeed = 8,
-        overdueState = 115000,
-        fridgeEfficiency = 6,
-        saltEfficiency = 4,
+    val AVOCADO = config {
+        overdueSpeed = 8
+        overdueState = 115000
+        fridgeEfficiency = 6
+        saltEfficiency = 4
         category = FoodCategory.FRUIT
-    )
+    }
 
-    val BAKED_BEANS = FoodConfig(
-        overdueSpeed = 5,
-        overdueState = 150000,
-        fridgeEfficiency = 2,
-        saltEfficiency = 1,
+    val BAKED_BEANS = config {
+        overdueSpeed = 5
+        overdueState = 150000
+        fridgeEfficiency = 2
+        saltEfficiency = 1
         category = FoodCategory.PLANT
-    )
+    }
 
-    val BANANA = FoodConfig(
-        overdueSpeed = 7,
-        overdueState = 120000,
-        fridgeEfficiency = 4,
+    val BANANA = config {
+        overdueSpeed = 7
+        overdueState = 120000
+        fridgeEfficiency = 4
         category = FoodCategory.FRUIT
-    )
+    }
 
-    val BANANA_CREAM_PIE = FoodConfig(
-        overdueSpeed = 9,
-        overdueState = 140000,
-        fridgeEfficiency = 5,
+    val BANANA_CREAM_PIE = config {
+        overdueSpeed = 9
+        overdueState = 140000
+        fridgeEfficiency = 5
         category = FoodCategory.PRODUCT
-    )
+    }
 
-    val BANANA_NUT_BREAD = FoodConfig(
-        overdueSpeed = 10,
-        overdueState = 165000,
-        fridgeEfficiency = 5,
-        saltEfficiency = 7,
+    val BANANA_NUT_BREAD = config {
+        overdueSpeed = 10
+        overdueState = 165000
+        fridgeEfficiency = 5
+        saltEfficiency = 7
         category = FoodCategory.PRODUCT
-    )
+    }
 
-    val BARLEY = FoodConfig(
-        overdueSpeed = 8,
-        overdueState = 130000,
-        fridgeEfficiency = 4,
+    val BARLEY = config {
+        overdueSpeed = 8
+        overdueState = 130000
+        fridgeEfficiency = 4
         category = FoodCategory.PLANT
-    )
+    }
 
-    val BASIL = FoodConfig(
-        overdueSpeed = 7,
-        overdueState = 90000,
-        fridgeEfficiency = 3,
-        saltEfficiency = 4,
+    val BASIL = config {
+        overdueSpeed = 7
+        overdueState = 90000
+        fridgeEfficiency = 3
+        saltEfficiency = 4
         category = FoodCategory.PLANT
-    )
+    }
 
-    val BEEF_JERKY = FoodConfig(
-        rotSpeed = 8,
-        rotState = 140000,
-        fridgeEfficiency = 5,
-        saltEfficiency = 3,
+    val BEEF_JERKY = config {
+        rotSpeed = 8
+        rotState = 140000
+        fridgeEfficiency = 5
+        saltEfficiency = 3
         category = FoodCategory.MEAT
-    )
+    }
 
-    val BEEF_WELLINGTON = FoodConfig(
-        rotSpeed = 7,
-        rotState = 150000,
-        fridgeEfficiency = 4,
-        saltEfficiency = 3,
+    val BEEF_WELLINGTON = config {
+        rotSpeed = 7
+        rotState = 150000
+        fridgeEfficiency = 4
+        saltEfficiency = 3
         category = FoodCategory.MEAT
-    )
+    }
 
-    val BELLPEPPER = FoodConfig(
-        overdueSpeed = 9,
-        overdueState = 115000,
-        fridgeEfficiency = 4,
+    val BELLPEPPER = config {
+        overdueSpeed = 9
+        overdueState = 115000
+        fridgeEfficiency = 4
         category = FoodCategory.VEGETABLE
-    )
+    }
 
-    val BLACKBEAN = FoodConfig(
-        overdueSpeed = 6,
-        overdueState = 70000,
-        fridgeEfficiency = 2,
-        saltEfficiency = 3,
+    val BLACKBEAN = config {
+        overdueSpeed = 6
+        overdueState = 70000
+        fridgeEfficiency = 2
+        saltEfficiency = 3
         category = FoodCategory.PLANT
-    )
+    }
 
-    val BLACKBERRY = FoodConfig(
-        overdueSpeed = 8,
-        overdueState = 120000,
-        fridgeEfficiency = 6,
+    val BLACKBERRY = config {
+        overdueSpeed = 8
+        overdueState = 120000
+        fridgeEfficiency = 6
         category = FoodCategory.BERRIES
-    )
+    }
 
-    val BLACKBERRY_JAM = FoodConfig(
-        overdueSpeed = 9,
-        overdueState = 160000,
-        fridgeEfficiency = 5,
+    val BLACKBERRY_JAM = config {
+        overdueSpeed = 9
+        overdueState = 160000
+        fridgeEfficiency = 5
         category = FoodCategory.PRODUCT
-    )
+    }
 
-    val BLT = FoodConfig(
-        overdueSpeed = 14,
-        overdueState = 105000,
-        fridgeEfficiency = 5,
+    val BLT = config {
+        overdueSpeed = 14
+        overdueState = 105000
+        fridgeEfficiency = 5
         category = FoodCategory.PRODUCT
-    )
+    }
 
-    val BLUEBERRY = FoodConfig(
-        overdueSpeed = 7,
-        overdueState = 125000,
-        fridgeEfficiency = 3,
+    val BLUEBERRY = config {
+        overdueSpeed = 7
+        overdueState = 125000
+        fridgeEfficiency = 3
         category = FoodCategory.BERRIES
-    )
+    }
 
-    val BLUEBERRY_JAM = FoodConfig(
-        overdueSpeed = 9,
-        overdueState = 145000,
-        fridgeEfficiency = 5,
+    val BLUEBERRY_JAM = config {
+        overdueSpeed = 9
+        overdueState = 145000
+        fridgeEfficiency = 5
         category = FoodCategory.PRODUCT
-    )
+    }
 
-    val BROCCOLI = FoodConfig(
-        overdueSpeed = 5,
-        overdueState = 75000,
-        fridgeEfficiency = 2,
-        saltEfficiency = 3,
+    val BROCCOLI = config {
+        overdueSpeed = 5
+        overdueState = 75000
+        fridgeEfficiency = 2
+        saltEfficiency = 3
         category = FoodCategory.PLANT
-    )
+    }
 
-    val BROWNIES = FoodConfig(
-        overdueSpeed = 12,
-        overdueState = 130000,
-        fridgeEfficiency = 5,
+    val BROWNIES = config {
+        overdueSpeed = 12
+        overdueState = 130000
+        fridgeEfficiency = 5
         category = FoodCategory.PRODUCT
-    )
+    }
 
-    val BURRITO = FoodConfig(
-        overdueSpeed = 8,
-        overdueState = 110000,
-        fridgeEfficiency = 4,
+    val BURRITO = config {
+        overdueSpeed = 8
+        overdueState = 110000
+        fridgeEfficiency = 4
         category = FoodCategory.PRODUCT
-    )
+    }
 
-    val BUTTER = FoodConfig(
-        overdueSpeed = 9,
-        overdueState = 80000,
-        fridgeEfficiency = 6,
+    val BUTTER = config {
+        overdueSpeed = 9
+        overdueState = 80000
+        fridgeEfficiency = 6
         category = FoodCategory.PRODUCT
-    )
+    }
 
-    val BUTTERED_TOAST = FoodConfig(
-        overdueSpeed = 13,
-        overdueState = 150000,
-        fridgeEfficiency = 7,
+    val BUTTERED_TOAST = config {
+        overdueSpeed = 13
+        overdueState = 150000
+        fridgeEfficiency = 7
         category = FoodCategory.PRODUCT
-    )
+    }
 
-    val CABBAGE = FoodConfig(
-        overdueSpeed = 8,
-        overdueState = 95000,
-        fridgeEfficiency = 2,
-        saltEfficiency = 5,
+    val CABBAGE = config {
+        overdueSpeed = 8
+        overdueState = 95000
+        fridgeEfficiency = 2
+        saltEfficiency = 5
         category = FoodCategory.VEGETABLE
-    )
+    }
 }
