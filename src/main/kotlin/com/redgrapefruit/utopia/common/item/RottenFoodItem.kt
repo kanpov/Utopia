21c99b1fad10539747c49f0613e719d2fea46545
RedGrapefruit <karpovanton729@gmail.com>
2021-08-11 20:03:06 +0300

    Port all variants

--- src/main/kotlin/com/redgrapefruit/utopia/common/item/RottenFoodItem.kt
+++ src/main/kotlin/com/redgrapefruit/utopia/common/item/RottenFoodItem.kt
@@ -1,11 +1,13 @@
 package com.redgrapefruit.utopia.common.item
 
+import com.mojang.datafixers.util.Pair
 import com.redgrapefruit.utopia.common.GROUP
 import com.redgrapefruit.utopia.common.RANDOM
 import com.redgrapefruit.utopia.common.core.FoodCategory
 import com.redgrapefruit.utopia.common.core.FoodConfig
 import com.redgrapefruit.utopia.common.core.FoodState
 import com.redgrapefruit.utopia.common.util.overrideComponentValues
+import com.redgrapefruit.utopia.mixin.FoodComponentAccessor
 import net.minecraft.entity.effect.StatusEffectInstance
 import net.minecraft.entity.effect.StatusEffects
 import net.minecraft.item.FoodComponent
@@ -13,50 +15,28 @@ import net.minecraft.item.FoodComponent
 /**
  * A rotten variant of a [FoodItem]
  */
-class RottenFoodItem : FoodItem {
-    constructor(config: FoodConfig) : super(config, GROUP, {
-        val builder = FoodComponent.Builder()
-
-        // Hunger (decreased)
-        builder.hunger(config.category.baseHunger + config.hunger - 2)
-        // Meat
-        if (config.category == FoodCategory.MEAT) builder.meat()
-        // Snack
-        if (config.category.baseHunger + config.hunger - 2 < 2) builder.snack()
-        // Saturation modifier (decreased)
-        builder.saturationModifier((config.category.baseSaturationModifier + config.saturationModifier) / 1.5f)
-        // Poison effect
-        builder.statusEffect(
-            StatusEffectInstance(
-                StatusEffects.POISON,
-                RANDOM.nextInt(250) + 50,
-                RANDOM.nextInt(3) + 1
-            ), 0.85f
-        )
-
-        builder.build()
-    }) {
+class RottenFoodItem(name: String) : FoodItem(name) {
+    init {
         state = FoodState.ROTTEN
         overrideEffects = true
     }
 
-    constructor(config: FoodConfig, component: FoodComponent) : super(config, GROUP, {
-        val builder = FoodComponent.Builder()
-
-        // Overrides
-        overrideComponentValues(component, builder, 2, 0.75f)
-        // Poison effect
-        builder.statusEffect(
-            StatusEffectInstance(
-                StatusEffects.POISON,
-                RANDOM.nextInt(250) + 50,
-                RANDOM.nextInt(3) + 1
-            ), 0.85f
-        )
-
-        builder.build()
-    }) {
-        state = FoodState.ROTTEN
-        overrideEffects = true
+    override fun onComponentInit(access: FoodComponentAccessor, component: FoodComponent) {
+        // Hunger (decreased)
+        access.setHunger(config.category.baseHunger + config.hunger - 2)
+        // Meat
+        if (config.category == FoodCategory.MEAT) access.setMeat(true)
+        // Snack
+        if (config.category.baseHunger + config.hunger - 2 < 2) access.setSnack(true)
+        // Saturation modifier (decreased)
+        access.setSaturationModifier((config.category.baseSaturationModifier + config.saturationModifier) / 1.5f)
+        // Effects
+        access.setStatusEffects(component.statusEffects.apply {
+            add(Pair.of(StatusEffectInstance(
+                    StatusEffects.POISON,
+                    RANDOM.nextInt(250) + 50,
+                    RANDOM.nextInt(3) + 1
+                ), 0.85f))
+        })
     }
 }
