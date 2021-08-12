7b3d5d65701e9f4265d871d20aa6e42a80e0d698
RedGrapefruit <karpovanton729@gmail.com>
2021-08-12 12:23:21 +0300

    Move MutableFoodComponent

--- src/main/kotlin/com/redgrapefruit/utopia/common/util/misc.kt
+++ src/main/kotlin/com/redgrapefruit/utopia/common/util/misc.kt
@@ -1,8 +1,10 @@
 package com.redgrapefruit.utopia.common.util
 
+import com.mojang.datafixers.util.Pair
 import com.redgrapefruit.utopia.common.core.FoodConfig
 import com.redgrapefruit.utopia.common.core.FoodProfile
 import com.redgrapefruit.utopia.common.core.FridgeState
+import net.minecraft.entity.effect.StatusEffectInstance
 import net.minecraft.item.FoodComponent
 import net.minecraft.text.LiteralText
 import net.minecraft.text.Text
@@ -95,3 +97,40 @@ fun calculateOverdue(profile: FoodProfile, config: FoodConfig): Int {
     }
     return out
 }
+
+/**
+ * Accessible [FoodComponent] implementation
+ */
+data class MutableFoodComponent(
+    var hunger: Int,
+    var saturationModifier: Float,
+    var meat: Boolean,
+    var alwaysEdible: Boolean,
+    var snack: Boolean,
+    var statusEffects: List<Pair<StatusEffectInstance, Float>>
+)
+
+// Conversion extensions
+
+/**
+ * Converts a [MutableFoodComponent] to a [FoodComponent]
+ */
+fun MutableFoodComponent.asImmutable(): FoodComponent {
+    val builder = FoodComponent.Builder()
+
+    builder.hunger(hunger)
+    builder.saturationModifier(saturationModifier)
+    if (meat) builder.meat()
+    if (alwaysEdible) builder.alwaysEdible()
+    if (snack) builder.snack()
+    statusEffects.forEach { pair -> builder.statusEffect(pair.first, pair.second) }
+
+    return builder.build()
+}
+
+/**
+ * Converts a [FoodComponent] to a [MutableFoodComponent]
+ */
+fun FoodComponent.asMutable(): MutableFoodComponent = MutableFoodComponent(
+    hunger, saturationModifier, isMeat, isAlwaysEdible, isSnack, statusEffects
+)
