30f5d3c13432915529041542ad35ef07e575a34e
RedGrapefruit <karpovanton729@gmail.com>
2021-08-16 15:04:22 +0300

    Poison the player when drinking rancid drinks

--- src/main/kotlin/com/redgrapefruit/utopia/compatibility/DehydrationCompatibility.kt
+++ src/main/kotlin/com/redgrapefruit/utopia/compatibility/DehydrationCompatibility.kt
@@ -1,11 +1,30 @@
 package com.redgrapefruit.utopia.compatibility
 
+import com.redgrapefruit.utopia.item.RancidDrinkItem
 import com.redgrapefruit.utopia.registry.DrinkRegistry
 import net.dehydration.api.DehydrationAPI
+import net.minecraft.entity.effect.StatusEffectInstance
+import net.minecraft.entity.effect.StatusEffects
 import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.item.ItemStack
+import kotlin.random.Random
 
 object DehydrationCompatibility : DehydrationAPI {
+    override fun onDrink(stack: ItemStack, playerEntity: PlayerEntity) {
+        super.onDrink(stack, playerEntity)
+
+        // Poison the player when drinking rancid stuff
+        if (stack.item is RancidDrinkItem) {
+            playerEntity.addStatusEffect(
+                StatusEffectInstance(
+                    StatusEffects.POISON,
+                    Random.nextInt(150, 301),
+                    Random.nextInt(0, 3)
+                )
+            )
+        }
+    }
+
     override fun calculateDrinkThirst(stack: ItemStack, playerEntity: PlayerEntity): Int {
         return DrinkRegistry.getThirstFor(stack.item)
     }
