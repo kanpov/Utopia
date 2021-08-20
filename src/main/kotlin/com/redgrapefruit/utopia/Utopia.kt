fb4f9d0293ea5464851de3b03ac0b30d057a2833
RedGrapefruit <karpovanton729@gmail.com>
2021-08-20 13:15:55 +0300

    Modify GApple and EGApple tooltips

--- src/main/kotlin/com/redgrapefruit/utopia/Utopia.kt
+++ src/main/kotlin/com/redgrapefruit/utopia/Utopia.kt
@@ -2,8 +2,13 @@ package com.redgrapefruit.utopia
 
 import com.redgrapefruit.utopia.registry.*
 import com.redgrapefruit.utopia.util.FoodConfigReloader
+import com.redgrapefruit.utopia.util.GOLD
+import com.redgrapefruit.utopia.util.breakLine
 import com.redgrapefruit.utopia.util.loadConfig
 import net.fabricmc.api.ModInitializer
+import net.fabricmc.fabric.api.client.item.v1.ItemTooltipCallback
+import net.minecraft.item.Items
+import net.minecraft.text.LiteralText
 import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
 
@@ -13,6 +18,14 @@ class Utopia : ModInitializer {
     override fun onInitialize() {
         FoodConfigReloader.register()
         loadConfig()
+
+        // Marks GApple and GodApple as resistant
+        ItemTooltipCallback.EVENT.register { stack, _, lines ->
+            if (stack.item == Items.GOLDEN_APPLE || stack.item == Items.ENCHANTED_GOLDEN_APPLE) {
+                breakLine(lines)
+                lines += LiteralText(GOLD + "Completely resistant to all food damage")
+            }
+        }
 
         FoodRegistry.run()
         DrinkRegistry.run()
